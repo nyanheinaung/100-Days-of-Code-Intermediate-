8c35a8c041cce698a194c18b307cf3d4fde2d3f6
Jack Star <nyanheinaung@gmail.com>
2024-09-18 23:52:59 +0900

    smtp connection needs to be checked again

--- day-32-sending-email-and-managing-dates/main.py
+++ day-32-sending-email-and-managing-dates/main.py
@@ -1,4 +1,9 @@
-# This is test text
+import smtplib
 
-a = 1
-b = 2
+my_email = "testforlearning12@gmail.com"
+password = "hdix ewbu conu dizo "
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs="testforlearning12@yahoo.com", msg="Hello")
+connection.close()
