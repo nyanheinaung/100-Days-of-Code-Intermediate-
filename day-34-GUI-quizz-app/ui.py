527592477b731ef1c37d675f4d7ac13f9a23db0a
Jack Star <nyanheinaung@gmail.com>
2024-09-30 23:58:35 +0900

    Finish TRIVIA question API call

--- day-34-GUI-quizz-app/ui.py
+++ day-34-GUI-quizz-app/ui.py
@@ -1,2 +1,24 @@
+import tkinter as tk
+
+FONT = ("Ariel", 20, "italic")
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = tk.Label(text=f"Score: {0}", bg=THEME_COLOR, fg="white")
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = tk.Canvas(height=250, width=300, bg="white")
+        self.canvas.create_text((125,150), text="Question Text", font=FONT)
+        self.canvas.grid(row=1, column=0, columnspan=2)
+
+        true_img = tk.PhotoImage("images/true.png")
+
+        false_img = tk.PhotoImage("images/false.png")
+
+
+        self.window.mainloop()
